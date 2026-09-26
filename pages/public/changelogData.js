7b67a7562b749a63332f8855aa37751013ad99c5
KushalRoyChowdhury <kushalr901@gmail.com>
2026-09-26 08:41:34 +0530

    CHANGELOG
    Signed-off-by: KushalRoyChowdhury <kushalr901@gmail.com>

--- pages/public/changelogData.js
+++ pages/public/changelogData.js
@@ -4,7 +4,7 @@ export const changelogData = {
     "4.20.0": {
         "note": "SEPTEMBER 26",
         "added": [
-            "Added conversation context for `fluxflow -p ‹prompt›`. Context will be retained till 10m of inactivity. To start a clean turn without context, use `--new` (alias `-n`) flag."
+            "Added conversation context for `fluxflow -p ‹prompt›`. Context will be retained till 10m of inactivity or 69 turns. To start a clean turn without context, use `--new` (alias `-n`) flag."
         ],
         "changed": [],
         "fixes": [
